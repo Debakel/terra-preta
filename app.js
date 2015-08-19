dc1967537642691f637b7a08b2096a9bd4f36653
Debakel <git@mrtz.me>
2015-08-19 03:48:12 +0200

    Added app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+function loadMap(){
+			var map = L.map('map').setView([51.505, -0.09], 13);
+		L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+			attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+			maxZoom: 18,
+			id: 'debakel.in6i4ino',
+			accessToken: 'pk.eyJ1IjoiZGViYWtlbCIsImEiOiJjMWVJWEdFIn0.WtaUd8Rh0rgHRiyEZNzSjQ'
+		}).addTo(map);
+		
+		L.control.locate().addTo(map);
+		var sidebar = L.control.sidebar('sidebar').addTo(map);
+}
+window.onload = loadMap();   
