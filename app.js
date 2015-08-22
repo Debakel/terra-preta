26f1a0231dafc00f89885739027037d8c851964f
Debakel <git@mrtz.me>
2015-08-22 23:08:57 +0200

    bower & grunt test

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
+var map;
+function onload(){
+	L.mapbox.accessToken = 'pk.eyJ1IjoiZGViYWtlbCIsImEiOiJjMWVJWEdFIn0.WtaUd8Rh0rgHRiyEZNzSjQ';
+	loadMap();
+	JSONload();
+}
 function loadMap(){
-			var map = L.map('map').setView([51.505, -0.09], 13);
+		map = L.map('map').setView([51.505, -0.09], 13);
 		L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
 			attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
 			maxZoom: 18,
@@ -10,4 +16,10 @@ function loadMap(){
 		L.control.locate().addTo(map);
 		var sidebar = L.control.sidebar('sidebar').addTo(map);
 }
-window.onload = loadMap();   
+function JSONload(){
+	var featureLayer = L.mapbox.featureLayer()
+    .loadURL('top100.geojson')
+    .addTo(map);
+}
+
+window.onload = onload();   
