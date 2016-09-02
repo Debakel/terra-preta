ff846c17ae5c772d3dac6c2d460475efc1071226
Moritz Duchene <duchenmo@y2015w04.student.uni-augsburg.de>
2016-09-02 12:26:25 +0200

    year as own field

--- preprocessing.py
+++ preprocessing.py
@@ -1,7 +1,15 @@
 import json
+import re
 
 findings = json.load(open('data/findings.json'))
 sites = json.load(open('data/excavation_sites.json'))
+
+def move_year_to_own_field():
+    for finding in findings['features']:
+	archaelogist = finding['properties']['archaeologist_name']
+	m = re.search("[0-9]{4}", archaelogist)
+	if m:
+	    finding['properties']['year'] = m.group()
 
 def get_findings_for_site(site_id):
     filtered_findings = list()
@@ -10,6 +18,7 @@ def get_findings_for_site(site_id):
             filtered_findings.append(finding['properties'])
     return filtered_findings
 
+move_year_to_own_field()
 # Add findings to excavation sites
 for site in sites['features']:
     site_id = site['properties']['excavation_site_id_pk']
